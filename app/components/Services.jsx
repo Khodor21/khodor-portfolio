b21cd2915e5b80ae9d1670590168154cbb91c2ce
Khodor Hasan <khodorhasan17@gmail.com>
2023-12-11 07:25:04 +0200

    Finish servies section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,26 +1,80 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
+import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import Title from "../assets/ServicesTitle.svg";
-import Service1 from "../assets/Service1.svg";
-import Service2 from "../assets/Service2.svg";
-import Service3 from "../assets/Service3.svg";
+import WebAppDevelopment from "../assets/WebAppDevelopment.svg";
+import SocialMediaDesign from "../assets/SocialMediaDesign.svg";
+import AdvertisementsDesign from "../assets/AdvertisementsDesign.svg";
+import NextJs from "../assets/Nextjs.svg";
+import Nodejs from "../assets/Nodejs.svg";
+import MongoDB from "../assets/MongoDB.svg";
+import Figma from "../assets/Figma.svg";
+import Blender from "../assets/Blender.svg";
+import AdobePremiere from "../assets/Adobe Premiere.svg";
+import RightArrow from "../assets/RightArrow.svg";
 
-const Services = () => {
-  // Animation variants for each service card
+const ServiceCard = ({ image, title, description, icons }) => {
   const cardVariants = {
     hidden: { opacity: 0, scale: 0.8 },
     visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
   };
 
   return (
+    <motion.div
+      className="border bg-white p-6 h-full"
+      variants={cardVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <Image
+        src={image}
+        priority
+        alt=""
+        className="border object-contain h-48 mx-auto"
+      />
+      <h3 className="mb-2 text-third text-lg" id="arabic">
+        {title}
+      </h3>
+      <p id="ibmsemi" className="text-third/70 text-base">
+        {description}
+      </p>
+      <div className="flex flex-col sm:flex-row gap-2 pt-6">
+        <div className="flex flex-wrap gap-2 justify-center sm:justify-start">
+          {icons.map((icon, index) => (
+            <div key={index} className="bg-third rounded-full p-2 mb-2 sm:mb-0">
+              <Image src={icon} alt="" className="h-6 w-6" />
+            </div>
+          ))}
+        </div>
+        <div className="flex items-center justify-center mt-2 sm:mt-0 sm:ml-auto sm:order-first">
+          <Link href="/portfolio">
+            <div className="flex items-center justify-center text-third">
+              <p className="mr-1 mb-1 " id="arabic">
+                انتقل إلى معرض الأعمال
+              </p>
+              <Image
+                src={RightArrow}
+                alt="Arrow Right"
+                width={20}
+                height={20}
+              />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+const Services = () => {
+  return (
     <div className="text-center bg-third pb-4">
       <div className="flex justify-end">
         <Image
           src={Title}
           priority
-          alt="Texture"
+          alt="Services Title"
           className="w-48 md:w-80 pt-6 mx-8"
         />
       </div>
@@ -29,75 +83,30 @@ const Services = () => {
         className="mb-6 mt-2 text-main text-right mx-10 md:text-lg"
         id="ibmbold"
       >
-        خدمات متميزة تســــاعدك علــى تحقيـق أهدافك وتطوير مشروعك بكفاءة عالية{" "}
+        خدمات متميزة تســــاعدك علــى تحقيـق أهدافك وتطوير مشروعك بكفاءة عالية
       </h3>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center mb-6 mx-8">
-        {/* Service 1 */}
-        <motion.div
-          className="border bg-white p-6 h-full"
-          variants={cardVariants}
-          initial="hidden"
-          animate="visible"
-        >
-          <Image
-            src={Service1}
-            priority
-            alt=""
-            className="border border-b-white object-contain h-48 mx-auto"
-          />
-          <h3 className="mb-2 text-third text-lg" id="arabic">
-            برمــــــجة تطبيقــــــات الويـــــــب
-          </h3>
-          <p id="ibmsemi" className="text-third text-base">
-            تطوير تطبيقات الويب باستخدام أحدث التقنيات. نقدم حلولًا مخصصة
-            وفعّالة لتلبية احتياجات عملك
-          </p>
-        </motion.div>
+        <ServiceCard
+          image={WebAppDevelopment}
+          title="برمــــــجة تطبيقــــــات الويـــــــب"
+          description="تطوير تطبيقات الويب باستخدام أحدث التقنيات. أُقــدّم حلولًا مخصصة وفعّالة لتلبية احتياجات عملك"
+          icons={[NextJs, Nodejs, MongoDB]}
+        />
 
-        {/* Service 2 */}
-        <motion.div
-          className="border bg-white p-6 h-full"
-          variants={cardVariants}
-          initial="hidden"
-          animate="visible"
-        >
-          <Image
-            src={Service2}
-            priority
-            alt=""
-            className="border object-contain h-48 mx-auto"
-          />
-          <h3 className="mb-2 text-third text-lg" id="arabic">
-            تصــــــاميـــم ســــوشــل ميديـــــــا
-          </h3>
-          <p id="ibmsemi" className="text-third text-base">
-            تصميم محتوى جذاب لوسائط التواصل الاجتماعي. نساعدك في بناء هوية قوية
-            على منصات التواصل الاجتماعي
-          </p>
-        </motion.div>
+        <ServiceCard
+          image={SocialMediaDesign}
+          title="تصــــــاميـــم ســــوشــل ميديـــــــا"
+          description="تصميم محتوى جذاب لوسائط التواصل الاجتماعي. أُساعدك في بناء هوية قوية على منصات التواصل الاجتماعي"
+          icons={[Figma]}
+        />
 
-        {/* Service 3 */}
-        <motion.div
-          className="border bg-white p-6 h-full"
-          variants={cardVariants}
-          initial="hidden"
-          animate="visible"
-        >
-          <Image
-            src={Service3}
-            priority
-            alt=""
-            className="border object-contain h-48 mx-auto"
-          />
-          <h3 className="mb-2 text-third text-lg" id="arabic">
-            تصميــــم فيديوهــــات إعلانيّــــــة
-          </h3>
-          <p id="ibmsemi" className="text-third text-base">
-            إنشاء فيديوهات إعلانية فاعلة ومبتكرة. نحن نقدم خدمات الإنتاج الفني
-            بجودة عالية لتعزيز علامتك التجارية
-          </p>
-        </motion.div>
+        <ServiceCard
+          image={AdvertisementsDesign}
+          title="تصميــــم فيديوهــــات إعلانيّــــــة"
+          description="إنشاء فيديوهات إعلانية فاعلة ومبتكرة. أُقدم خدمات الإنتاج الفني بجودة عالية لتعزيز علامتك التجارية"
+          icons={[Blender, AdobePremiere]}
+        />
       </div>
     </div>
   );
