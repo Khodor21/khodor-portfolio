0e1639f182bdc629f172fbc3b4b5581dbd7f3949
Khodor Hasan <khodorhasan17@gmail.com>
2023-12-18 11:03:17 +0200

    Edit SpeedInsights

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SpeedInsights>{children} </SpeedInsights>
+        {children} <SpeedInsights />
       </body>
     </html>
   );
