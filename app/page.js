05a5f5611849fbe4147072797ba8fc5b411ce251
KidsIslamicProjects <khodorhasan50@gmail.com>
2025-11-18 17:01:36 +0200

    Redirect

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Hero from "./sections/Hero";
 import Services from "./sections/Services";
 import Portfolio from "./sections/Portfolio";
 import ProcessSection from "./sections/process";
 
 const Page = () => {
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.push("/ar");
+  }, [router]);
+
   return (
     <div className="bg-white">
       <Hero />
